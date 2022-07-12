3895806f90de4b3965b8a3755cf02996373b01b3
Noel Ly <noel@Noel-Air.lan>
2022-07-12 08:26:07 -0700

    bs

---
+++ config/wdio.ios.bs.conf.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { config } = require('./wdio.shared.conf');
+
+// ============
+// Browserstalk
+// ============
+config.user= process.env.BROWSERSTACK_USER;
+config.key= process.env.BROWSERSTACK_KEY;
+
+// ============
+// Specs
+// ============
+
+config.specs = [
+    'test/specs/ios/ios-ToDoList-Screen-Objects-withHooks.spec.js'
+];
+
+// ============
+// Capabilities
+// ============
+config.capabilities = [
+    {
+        platformName: "ios",
+        "appium:platformVersion": "14.5",
+        "appium:deviceName": "iPhone 12",
+        "appium:automationName": "xcuitest",
+        "appium:app": path.join(process.cwd(), "./app/ios/MVCTodo.app"),
+        "appium:autoGrantPermissions": true
+    }   
+];
+config.services = ['browserstack'];
+
+exports.config = config;
